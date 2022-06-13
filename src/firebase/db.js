3c1d4b590e87bb9560574097c4749a0861dc6d1b
Hyuno Choi <soonitoon@gmail.com>
2022-06-13 17:06:31 +0900

    chore: Set ENV

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyAuje1eBx88tPuxXoshk0sF5OjhJPHq2ig",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "join-study.firebaseapp.com",
   projectId: "join-study",
   storageBucket: "join-study.appspot.com",
-  messagingSenderId: "1003577309856",
-  appId: "1:1003577309856:web:a6f6f1b351f9d76d18a7c5",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
